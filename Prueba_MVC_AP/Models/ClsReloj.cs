2a162074ccd0bc0e08385b6f194cdcb2c6bb64f0
Diego Porlles Carrillo <diego.porlles.22@gmail.com>
2020-06-09 23:41:01 -0500

    nueva clase

---
+++ Prueba_MVC_AP/Models/ClsReloj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba_MVC_AP.Models
+{
+    public class ClsReloj
+    {
+        int hora;
+        int minuto;
+    }
+}
